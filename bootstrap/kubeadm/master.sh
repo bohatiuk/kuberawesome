2e9682fba328b3a428be71d42f800a316e4d6e39
Anton <bbv98@mail.ru>
2022-03-08 22:54:50 +0100

    update master,worker scripts

--- bootstrap/kubeadm/master.sh
+++ bootstrap/kubeadm/master.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
-echo "--preflight checks"
+ERR=`tput setaf 1`
+TXT=`tput setaf 2`
+WARN=`tput setaf 3`
+RST=`tput sgr0`
+PORTS="                             
+6443 -> kubernetes API server       
+2379-2380 -> etcd server client API 
+10250 -> kubelet API                
+10259 -> kube-scheduler             
+10257 -> kube-controller-manager    
+6783 -> weave CNI                   
+"
+
+echo "$TXT--preflight checks$RST"
+echo "OS:"
 grep -i ubuntu /etc/issue
-if [ $? -ne 0 ]; then echo "ubuntu only supported"; exit 1; fi
-version=$1
-if [ -z "$version" ]; then version="latest"; echo "kubernetes version not supplied, using latest"; fi
-echo "kubernetes version $version-00 will be installed"
-echo "preflight checks--"
+if [ $? -ne 0 ]; then echo "${ERR}--ubuntu only supported--$RST"; exit 1; fi
+echo "${WARN}--following inbound ports must be open on the node: $PORTS--$RST"
+version="$1-00"
+if [ -z "$1" ]; then version="latest"; echo "--${WARN}kubernetes version not supplied, using latest--$RST"; fi
+echo "preflight checks--$RST"
 
-echo "--disabling swap"
+echo "$TXT--disabling swap$RST"
 swapoff -a
-echo "disabling swap--"
+echo "${TXT}disabling swap--$RST"
 
-echo "--installing cointainerd"
+echo "$TXT--installing cointainerd$RST"
 cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
 EOF
+echo "\n"
 sudo modprobe overlay && sudo modprobe br_netfilter
 cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
 net.bridge.bridge-nf-call-iptables  = 1
@@ -29,47 +44,46 @@ sudo apt-get install -y containerd
 sudo mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml
 sudo systemctl restart containerd
-echo "installing cointainerd--"
+echo "${TXT}installing cointainerd--$RST"
 
-echo "--containerd status"
+echo "$TXT--containerd status$RST"
 echo $(systemctl status containerd)
-echo "containerd status--"
+echo "${TXT}containerd status--$RST"
 
-echo "--installing kubelet, kubeadmin, kubectl @$version"
+echo "$TXT--installing kubelet, kubeadm, kubectl $RST"
 sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubelet="$version-00" kubeadm="$version-00" kubectl="$version-00"
+if [ "$version" = "latest" ]; then version=$(apt list -a | grep kubeadm | cut -d" " -f2 | head -n 1); fi
+sudo apt-get install -y kubelet="$version" kubeadm="$version" kubectl="$version"
 sudo apt-mark hold kubelet kubeadm kubectl
-echo "installing kubelet, kubeadmin, kubectl--"
+echo "${TXT}installing kubelet, kubeadm, kubectl @$version--$RST"
 
-echo "--initializing cluster"
+echo "$TXT--initializing cluster$RST"
 sudo kubeadm init
-echo "initializing cluster--"
+echo "${TXT}initializing cluster--$RST"
 
-echo "--saving default admin user kubeconfig to $HOME/.kube"
+echo "$TXT--saving default admin user kubeconfig to $HOME/.kube$RST"
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-echo "saving default admin user kubeconfig--"
+echo "${TXT}saving default admin user kubeconfig--$RST"
 
-echo "--installing weave-net CNI plugin"
+echo "$TXT--installing weave-net CNI plugin$RST"
 echo "note: incoming port 6783 must be opened"
 wget https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n') -O ~/weave.yaml
 kubectl apply -f ~/weave.yaml
 
 echo "check status of weave via:"
 echo "kubectl exec -n kube-system WEAVE_MASTER weave -- /home/weave/weave --local status"
-echo "installing weave-net CNI plugin--"
+echo "installing weave-net CNI plugin--$RST"
 
-echo "--adding bash completion"
-source <(kubectl completion bash)
+echo "$TXT--adding bash completion$RST"
+kubectl completion bash
 echo "source <(kubectl completion bash)" >> ~/.bashrc
-echo "adding bash completion--"
+echo "${TXT}adding bash completion--$RST"
 
-echo "--getting join command for worker"
+echo "$TXT--getting join command for worker$RST"
 kubeadm token create --print-join-command
-echo "getting join command for worker--"
-
-
+echo "${TXT}getting join command for worker--$RST"
